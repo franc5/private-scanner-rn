639737d19b7b6112c678f19a83a1913bb6e3881f
Francisco Costa <francosta@protonmail.com>
2023-02-19 10:20:34 -0300

    Detect sheet corners using opencv
    The corner detection logic was ported from https://github.com/franc5/private-scanner
    
    This still needs too much work

--- android/app/src/main/java/com/privatescanner/ImageProcessingModule.java
+++ android/app/src/main/java/com/privatescanner/ImageProcessingModule.java
@@ -1,12 +1,29 @@
 package com.privatescanner;
 
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
 
-import org.opencv.android.OpenCVLoader;
+import com.facebook.react.bridge.*;
+
+import org.opencv.android.Utils;
+import org.opencv.core.*;
+import static org.opencv.imgproc.Imgproc.*;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Vector;
 
 public class ImageProcessingModule extends ReactContextBaseJavaModule {
+    static {
+        System.loadLibrary("opencv_java4");
+    }
+
     ImageProcessingModule(ReactApplicationContext context) {
         super(context);
     }
@@ -16,12 +33,92 @@ public class ImageProcessingModule extends ReactContextBaseJavaModule {
         return "ImageProcessingModule";
     }
 
-    @ReactMethod(isBlockingSynchronousMethod = true)
-    public String sayHello() {
-        if (OpenCVLoader.initDebug()) {
-            return "Hello from Image Processing Native Module: OpenCV is working! =)";
-        } else {
-            return "Hello from Image Processing Native Module: OpenCV is not working! =(";
+    private MatOfPoint2f MatOfPointToMatOfPoint2f(MatOfPoint input) {
+        return new MatOfPoint2f(input.toArray());
+    }
+
+    private Mat loadImage(String imageUri) throws IOException {
+        final Uri uri = Uri.parse(imageUri);
+        final ContentResolver resolver = Objects.requireNonNull(getCurrentActivity()).getContentResolver();
+
+        Bitmap bitmap = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+                ? ImageDecoder.decodeBitmap(ImageDecoder.createSource(resolver, uri))
+                : MediaStore.Images.Media.getBitmap(resolver, uri);
+        bitmap = bitmap.copy(Bitmap.Config.RGB_565, false);
+
+        Mat image = new Mat();
+        Utils.bitmapToMat(bitmap, image);
+
+        return image;
+    }
+
+    private MatOfPoint getLargestContour(Mat input) {
+        List<MatOfPoint> contours = new Vector<>();
+        findContours(input, contours, new Mat(), RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        return Collections.max(contours, (c1, c2) -> {
+            double perimeter1 = arcLength(MatOfPointToMatOfPoint2f(c1), true);
+            double perimeter2 = arcLength(MatOfPointToMatOfPoint2f(c2), true);
+            return Double.compare(perimeter1, perimeter2);
+        });
+    }
+
+    private MatOfPoint getCurveConvexHull(MatOfPoint curve) {
+        MatOfInt hullPointIdxs = new MatOfInt();
+        convexHull(curve, hullPointIdxs);
+
+        final Point[] curvePoints = curve.toArray();
+        final List<Point> hullPoints = new Vector<>();
+        hullPointIdxs.toList().forEach(idx -> hullPoints.add(curvePoints[idx]));
+
+        MatOfPoint hull = new MatOfPoint();
+        hull.fromList(hullPoints);
+        return hull;
+    }
+
+    private MatOfPoint2f getSheetCorners(MatOfPoint curve, int maxIterations) {
+        if (curve.rows() < 4) throw new Error("Invalid curve");
+        if (curve.rows() == 4) return MatOfPointToMatOfPoint2f(curve);
+
+        int iterations = 0;
+        MatOfPoint hull = getCurveConvexHull(curve);
+        if (hull.rows() == 4) return MatOfPointToMatOfPoint2f(hull);
+
+        MatOfPoint2f hull2f = MatOfPointToMatOfPoint2f(hull);
+        MatOfPoint2f approximatedCurve = new MatOfPoint2f();
+        double epsilon = 0.02 * arcLength(hull2f, true);
+
+        while (approximatedCurve.rows() != 4 && iterations < maxIterations) {
+            approxPolyDP(hull2f, approximatedCurve, epsilon, true);
+            if (approximatedCurve.rows() < 4) epsilon *= 0.8;
+            else epsilon *= 1.2;
+            iterations++;
         }
+
+        if (approximatedCurve.rows() != 4) throw new Error("Cannot approximate curve");
+
+        return approximatedCurve;
+    }
+
+    // TODO: Avoid blocking methods
+    @ReactMethod(isBlockingSynchronousMethod = true)
+    public String detectSheetCorners(String photoUri) throws IOException {
+        Mat img = loadImage(photoUri);
+
+        // TODO: Investigate a way to automatically set the threshold and ratio based on the image
+        Mat edges = new Mat();
+        cvtColor(img, edges, COLOR_RGBA2GRAY);
+        Canny(edges, edges, 200, 2 * 200);
+
+        MatOfPoint sheetContour = getLargestContour(edges);
+        Point[] sheetCorners = getSheetCorners(sheetContour, 25).toArray();
+        // TODO: Sort corners from top-left to bottom-right
+
+        // TODO: Return a proper object
+        return ""
+                + (int) sheetCorners[0].x + "," + (int) sheetCorners[0].y + ";"
+                + (int) sheetCorners[1].x + "," + (int) sheetCorners[1].y + ";"
+                + (int) sheetCorners[2].x + "," + (int) sheetCorners[2].y + ";"
+                + (int) sheetCorners[3].x + "," + (int) sheetCorners[3].y;
     }
 }
